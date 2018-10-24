7b956f83bb06952ef637bb37966bd13df7f00822
Yeferson <yefry1128@gmail.com>
2018-10-24 17:27:32 -0500

    updating first test case (#2)

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -3,5 +3,8 @@ import { Config } from 'protractor';
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub'
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+  }
 };

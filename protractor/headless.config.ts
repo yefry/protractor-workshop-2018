71a9d64c70cab70bfa3eb90f1265d27f0437e544
yeferson.marin <Yeferson.Marin@e-hps.com>
2018-10-29 14:38:58 -0500

    Chrome Headless

---
+++ protractor/headless.config.ts
@@ -0,0 +1,18 @@
+import { Config, browser } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: [ '../test/google.spec.js' ],
+  SELENIUM_PROMISE_MANAGER: false,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  }
+};

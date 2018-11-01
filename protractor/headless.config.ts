bb9b3559a788cf4e2297f3c054581d8cbc14067e
Yeferson <yefry1128@gmail.com>
2018-11-01 14:34:44 -0500

    Jasmine - timeout (#12)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,7 +4,10 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 1000,
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;

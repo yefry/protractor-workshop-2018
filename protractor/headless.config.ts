608303d3dd25e070d5fe97a1018da7d6051e1271
Yeferson <yefry1128@gmail.com>
2018-11-01 08:22:45 -0500

    Css selector (#9)
    * CSS Selector
    
    * CSS Selector

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,7 +3,8 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/google.spec.js'],
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 1000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;

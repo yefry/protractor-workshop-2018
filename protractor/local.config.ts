7b4e8fd19d12bcf9637f1d929a4b10678a7b6a32
yeferson.marin <Yeferson.Marin@e-hps.com>
2018-11-02 13:12:11 -0500

    explicit wait implementation

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -11,7 +11,7 @@ export const config: Config = {
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
-    browser.manage().timeouts().implicitlyWait(3000);
+    browser.manage().timeouts().implicitlyWait(0);
     reporter();
   }
 };

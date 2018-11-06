9d796278f5c7113c041afadd2a3cbd79356fc7a0
Yeferson <yefry1128@gmail.com>
2018-11-06 07:12:47 -0500

    explicit wait implementation (#14)

--- src/page/product-added-modal.page.ts
+++ src/page/product-added-modal.page.ts
@@ -1,4 +1,4 @@
-import { $, ElementFinder } from 'protractor';
+import { $, browser, ElementFinder, ExpectedConditions } from 'protractor';
 
 export class ProductAddedModalPage {
   private element: ElementFinder;
@@ -8,6 +8,7 @@ export class ProductAddedModalPage {
   }
 
   public async proceedToCheckout(): Promise<void> {
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.element), 3000);
     await this.element.click();
   }
 }
